73c3e53bb69bb4a28f868cc64dc1e55a2fc0593b
Sanjib <sanjib.php@gmail.com>
2018-04-12 04:30:12 -0400

    Added Total Project

---
+++ sites/default/settings.php
@@ -0,0 +1,17 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'Fp7tKOrWyZ8S48zk1gtJB0Z1hsMnal9Gx6qyB_CtN3x8cMQ-MWd1cFMO-jUGtQURS90nVtVhkA';
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_dq69W3_MjobaEIW7VOrRrjlfF1YTCJkkK79QGNvrG33KEIyRW5ga9vWCQpZFTM_0W5CuE5asUQ/sync';
+$settings['trusted_host_patterns'] = array(
+'drupal8.local',
+);
